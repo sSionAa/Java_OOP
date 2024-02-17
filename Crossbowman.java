acadb5175f5ee955f94f571f2926deddb8a9c85c
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-17 18:33:01 +0300

    Create Crossbowman.java

---
+++ Crossbowman.java
@@ -0,0 +1,10 @@
+package Java_OOP.Lection_1;
+
+public class Crossbowman extends Persons {
+    public Crossbowman(String name){
+        super(name, "Elf",10, 20, 2, 2, 8, 5, 1, "Arbalet");
+    }
+}
+
+
+
