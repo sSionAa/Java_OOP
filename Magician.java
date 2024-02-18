f396845728d37302a3d6ca2b8eefe890cbae15fe
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-18 14:48:59 +0300

    Update Magician.java

--- Magician.java
+++ Magician.java
@@ -2,7 +2,7 @@ package Java_OOP;
 
 public class Magician extends Persons {
     public Magician(String name, int x, int y){
-        super(x, y, name, "Human", 70, 100, 10, 10, 10, 6, 1, "Stick");
+        super(x, y, name, "Human", 70, 100, 10, 10, 10, 6, 1, "Wand");
 
     }
 }
