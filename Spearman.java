29ea897e08489e8bbba82e97b20cf7e225332645
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-18 14:54:26 +0300

    Update Spearman.java

--- Spearman.java
+++ Spearman.java
@@ -1,8 +1,7 @@
 package Java_OOP;
 
 public class Spearman extends Persons {
-    public Spearman(String name) {
-
-        super(name, "Elf", 20, 65, 6, 2, 5, 8, 1, "Spear");
+    public Spearman(String name, int x, int y) {
+        super(x, y, name, "Elf", 20, 65, 6, 2, 5, 8, 1, "Spear");
     }
 }
