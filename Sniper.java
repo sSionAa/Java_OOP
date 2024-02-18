3c348b6214dda8c5c0842717d706de08c1f22e8e
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-18 14:53:38 +0300

    Update Sniper.java

--- Sniper.java
+++ Sniper.java
@@ -1,7 +1,7 @@
 package Java_OOP;
 
 public class Sniper extends Persons {
-    public Sniper(String name){
-        super(name, "Goblin", 100, 100, 10, 10, 10, 10, 1, "Sniper");
+    public Sniper(String name, int x, int y){
+        super(x, y, name, "Goblin", 100, 100, 10, 10, 10, 10, 1, "Sniper Rifle");
     }
 }
