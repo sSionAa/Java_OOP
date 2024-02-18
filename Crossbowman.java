813487e52428dc7887d5b16b19e44f1d661c75b3
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-18 14:46:32 +0300

    Update Crossbowman.java

--- Crossbowman.java
+++ Crossbowman.java
@@ -1,8 +1,8 @@
 package Java_OOP.Lection_1;
 
 public class Crossbowman extends Persons {
-    public Crossbowman(String name){
-        super(name, "Elf",10, 20, 2, 2, 8, 5, 1, "Arbalet");
+    public Crossbowman(String name, int x, int y){
+        super(x, y, name, "Elf",10, 20, 2, 2, 8, 5, 1, "Crossbow");
     }
 }
 
