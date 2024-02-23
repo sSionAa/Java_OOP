2aeaf63546f78f6f34e60bc1132251e3904faadb
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-23 12:23:07 +0300

    Update Crossbowman.java

--- Crossbowman.java
+++ Crossbowman.java
@@ -1,10 +1,40 @@
-package Java_OOP.Lection_1;
+package Java_OOP;
 
 public class Crossbowman extends Persons {
-    public Crossbowman(String name, int x, int y){
-        super(x, y, name, "Elf",10, 20, 2, 2, 8, 5, 1, "Crossbow");
+    protected int arrows;
+    
+    public Crossbowman(String name, int x, int y, String nameTeam, int arrows){
+        super(x, y, name, nameTeam, "Elf", 30, 30, 8, 5, 1, 5, 1, "Crossbow");
+        this.arrows = arrows;
     }
-}
-
-
-
+    
+    public void getStep(ArrayList<Persons> team) {
+            if (Crossbowman.this.getHealth() > 0) {
+                if (Crossbowman.this.getArrows() > 0) {
+                    System.out.println(this.name + " атаковал!");
+                    toAttack(getMinDistance(team));
+                    this.arrows--;
+                    Crossbowman.this.setArrows(this.arrows);
+                } else {
+                    System.out.println("Give me more arrows!!!");
+                }
+            }
+        }
+    
+        private void setArrows(int arrows) {
+            this.arrows = arrows;
+        }
+    
+        private int getArrows() {
+            return this.arrows;
+        }
+    
+        @Override
+        public String toString() {
+            return "Имя героя: " + getName() + ", класс: " + getClass().getSimpleName() + "; Команда: " + getNameTeam()
+                    + "\nПринадлежность героя: " + getRace() + "; Текущее состояние здоровья: " + getHealth() + "; Стрелы: " + getArrows()
+                    + "; Координаты (" + pos.getX() + " : " + pos.getY() + ")" + "; Приоритет: " + getSpeeed()
+                    + "\n" + "-".repeat(20);
+        }
+    
+    }
