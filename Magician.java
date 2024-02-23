3f261fe4b86e9b3bf2ed82893ff5f4ad5e81f071
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-23 12:32:22 +0300

    Update Magician.java

--- Magician.java
+++ Magician.java
@@ -1,8 +1,16 @@
 package Java_OOP;
 
+import Java_OOP.Persons;
+
+import java.util.ArrayList;
+
 public class Magician extends Persons {
-    public Magician(String name, int x, int y){
-        super(x, y, name, "Human", 70, 100, 10, 10, 10, 6, 1, "Wand");
+    public Magician(String name,int x, int y, String nameTeam){
+        super(x,y,name, nameTeam,"Human", 60, 60, 10, 10, 10, 3, 1, "Wand");
 
     }
+    @Override
+    public void getStep(ArrayList<Persons> team) {
+    }
+
 }
