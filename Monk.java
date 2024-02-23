4fa177007420e34038722a86edba3f45d02c2fe7
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-23 12:24:16 +0300

    Update Monk.java

--- Monk.java
+++ Monk.java
@@ -1,7 +1,10 @@
 package Java_OOP;
 
 public class Monk extends Persons {
-    public Monk(String name, int x, int y) {
-        super(x, y, name, "Human", 5, 20, 2, 10, 3, 2, 1, "Stick");
+    public Monk(String name, int x, int y, String nameTeam) {
+        super(x,y,name, nameTeam,"Human",5, 25, 20, 10, 3, 1, 1, "Stick");
+    }
+    @Override
+    public void getStep(ArrayList<Persons> team) {
     }
 }
