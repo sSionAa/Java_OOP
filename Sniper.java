e772ba5814d2ce46e84bdc1b407ee03df2780c44
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-23 12:29:16 +0300

    Update Sniper.java

--- Sniper.java
+++ Sniper.java
@@ -1,7 +1,39 @@
 package Java_OOP;
 
+import java.util.ArrayList;
+
 public class Sniper extends Persons {
-    public Sniper(String name, int x, int y){
-        super(x, y, name, "Goblin", 100, 100, 10, 10, 10, 10, 1, "Sniper Rifle");
+    protected int arrows;
+    
+    public Sniper(String name, int x, int y, String nameTeam, int arrows){
+        super(x,y,name, nameTeam,"Goblin", 60, 30, 6, 1, 8, 5, 1, "Sniper Rifle");
+        this.arrows=arrows;
     }
+    public void getStep(ArrayList<Persons> team) {
+        if (Sniper.this.getHealth() > 0) {
+            if (Sniper.this.getArrows() > 0) {
+                System.out.println(this.name + " атаковал!");
+                toAttack(getMinDistance(team));
+                this.arrows--;
+                Sniper.this.setArrows(this.arrows);
+            } else {
+                System.out.println("Give me more arrows!!!");
+            }
+        }
+    }
+    private void setArrows(int arrows) {
+        this.arrows = arrows;
+    }
+    private int getArrows() {
+        return this.arrows;
+    }
+    @Override
+    public String toString() {
+        return "Имя героя: " + this.name+ ", класс: " + getClass().getSimpleName() + "; Команда: " + getNameTeam()
+                + "\nПринадлежность героя: " + this.race + "; Текущее состояние здоровья: " + this.health+ "; Стрелы: " + getArrows()
+                + "; Координаты (" + pos.X + " : " + pos.Y + ")"+ "; Приоритет: " + getSpeeed()
+                + "\n"+"-".repeat(20);
+    }
+
 }
+
