d2fd181d84151e195442657f38dc44c5cd864983
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-23 12:27:06 +0300

    Update Peasant.java

--- Peasant.java
+++ Peasant.java
@@ -1,8 +1,16 @@
 package Java_OOP;
 
+import Java_OOP.Persons;
+import java.util.ArrayList;
+
 public class Peasant extends Persons {
-    public Peasant(String name, int x, int y){
-        super(x, y, name, "Human", 10, 30, 2, 0, 1, 2, 1, "Axe");
+    public Peasant(String name,int x, int y, String nameTeam){
+        super(x,y,name, nameTeam,"Human", 30, 30, 5, 0, 1, 2, 1, "Axe");
 
     }
+    @Override
+    public void getStep(ArrayList<Persons> team) {
+
+    }
+
 }
