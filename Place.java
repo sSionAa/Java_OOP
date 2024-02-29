62858cdf25b9c40ec78f104ea4cf78c2eccfe9b1
sSionAa <138705920+sSionAa@users.noreply.github.com>
2024-02-29 14:44:38 +0300

    Update Place.java

--- Place.java
+++ Place.java
@@ -20,8 +20,20 @@ public class Place {
         return Y;
     }
 
-    public static double getDistance(int x1, int y1, int x2, int y2) {
-        return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+//     public static double getDistance(int x1, int y1, int x2, int y2) {
+//         return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+//     }
+
+// }
+     public static double getDistance(Place hero, Place target) {
+        return sqrt((hero.getX()- target.getX()) * (hero.getX()- target.getX()) + (hero.getY()- target.getY()) * (hero.getY()- target.getY()));
+    }
+
+    public static Place getDifference(Place hero,Place target) {
+        return new Place(hero.getX() - target.getX(), hero.getY() - target.Y);
+    }
+    public boolean equals (Place place){
+        return this.X == place.X && this.Y == place.Y;
     }
 
 }
